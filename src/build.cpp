9231d1bf5d3d70316c584f8e7744770b147e3c34
Berk Unver <berkunver03@gmail.com>
2023-07-05 21:20:05 -0400

    Got window to open

--- src/build.cpp
+++ src/build.cpp
@@ -1,8 +1,8 @@
 #include <windows.h>
 #include <direct.h>
 
-#define SYSTEM_LIBRARIES "user32.lib kernel32.lib WinMM.Lib GDI32.lib OpenGL32.lib shell32.lib"
-#define LINKER_OPTIONS "/link glfw/glfw3.lib " SYSTEM_LIBRARIES " /LIBPATH:../lib"
+#define SYSTEM_LIBRARIES "user32.lib kernel32.lib WinMM.Lib GDI32.lib OpenGL32.lib shell32.lib msvcrt.lib"
+#define LINKER_OPTIONS "/link glfw/glfw3.lib " SYSTEM_LIBRARIES " /LIBPATH:../lib /NODEFAULTLIB:library"
 #define HEADER_PATHS "/I../include"
 #define COMPILER_OUTPUT_DIRS "/Fd../build/stealth.pdb /Fe../build/stealth.exe /Fo../build/stealth.obj"
 #define SOURCE_FILES "main.cpp"
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <stdio.h>
+#include "glfw/glfw3.h"
+
+#define WINDOW_X 800
+#define WINDOW_Y 600
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 
 int main() {
-    printf("Hello, World.\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(WINDOW_X, WINDOW_Y, "Stealth", NULL, NULL);
+    if (!window) {
+        printf("Failed to open GLFW window.\n");
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glViewport(0, 0, WINDOW_X, WINDOW_Y);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);    
+    
+    while (!glfwWindowShouldClose(window)) {
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) break;
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
     return 0;
 }
