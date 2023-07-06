dc0845613283768f65ba42fc793d15289da7839a
Berk Unver <berkunver03@gmail.com>
2023-07-05 20:56:46 -0400

    Initialized repository.

---
+++ src/build.cpp
@@ -0,0 +1,19 @@
+#include <windows.h>
+#include <direct.h>
+
+#define SYSTEM_LIBRARIES "user32.lib kernel32.lib WinMM.Lib GDI32.lib OpenGL32.lib shell32.lib"
+#define LINKER_OPTIONS "/link glfw/glfw3.lib " SYSTEM_LIBRARIES " /LIBPATH:../lib"
+#define HEADER_PATHS "/I../include"
+#define COMPILER_OUTPUT_DIRS "/Fd../build/stealth.pdb /Fe../build/stealth.exe /Fo../build/stealth.obj"
+#define SOURCE_FILES "main.cpp"
+
+// build with "cl build.cpp"
+
+int main() {
+    CreateDirectoryA("../build", NULL);
+    int compiled = system("cl.exe /Zi /std:c++20 " COMPILER_OUTPUT_DIRS " " HEADER_PATHS " " SOURCE_FILES " " LINKER_OPTIONS);
+    if (!compiled) {
+        system("..\\build\\stealth.exe");
+    }
+    return 0;
+}
